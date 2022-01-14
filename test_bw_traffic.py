ac76c50b56830695653e252872e46780466a1455
Heinrich Kuttler <hnr@fb.com>
2022-01-14 21:48:08 +0100

    Some more analysis.

--- test_bw_traffic.py
+++ test_bw_traffic.py
@@ -12,7 +12,7 @@ class PB(enum.IntEnum):
     SOMETIMES_ZERO = 3  # Seldom present.
     SOMETIMES_ALSO_ZERO = 4  # Shows up with 3.
     CHECKSUM = 5
-    OTHER_CHECKSUM = 9  # Shows up with when 11 is nonempty.
+    NINE_VARINT = 9  # Shows up with when 11 is nonempty.
     OFTEN_EMPTY = 11  # Not always, present with 3.
     SOMETIMES_9 = 12  # Shows up with 3.
 
@@ -20,6 +20,9 @@ class PB(enum.IntEnum):
 class TestBWData:
     def test_traffic_data(self):
         no_storm_packet = 0
+        packet_with_nine = 0
+
+        print("")
 
         with open("traffic_data.tsv") as f:
             reader = csv.DictReader(f, delimiter="\t")
@@ -28,6 +31,19 @@ class TestBWData:
                 pb = proto.parse_proto(data)
 
                 assert pb[PB.ALWAYS_ONE] == 1
+
+                if PB.NINE_VARINT in pb:
+                    # This is some kind of request/response. In this
+                    # data it shows up 64 times, half with a storm packet
+                    # and half without.
+                    packet_with_nine += 1
+                    assert pb[PB.OFTEN_EMPTY]
+                    assert 0x5D3C2A == pb[PB.NINE_VARINT] >> 28
+                    # print("0x%x" % pb[PB.NINE_VARINT], pb[PB.OFTEN_EMPTY])
+                    # print(row["peer"], pb)
+
+                if pb[PB.OFTEN_EMPTY]:
+                    assert PB.NINE_VARINT in pb
 
                 if PB.STORM_PACKET in pb:
                     # Raises error if checksum is wrong.
@@ -39,8 +55,9 @@ class TestBWData:
 
                     assert PB.SOMETIMES_ZERO not in pb
                     assert PB.SOMETIMES_ALSO_ZERO not in pb
+                    assert PB.CHECKSUM in pb
 
-                    if PB.OTHER_CHECKSUM in pb:
+                    if PB.NINE_VARINT in pb:
                         # This is typically not the case.
                         assert pb[PB.OFTEN_EMPTY]
                     else:
@@ -54,8 +71,11 @@ class TestBWData:
                 no_storm_packet += 1
                 assert PB.SOMETIMES_ZERO in pb
                 assert PB.SOMETIMES_ALSO_ZERO in pb
-                assert PB.OTHER_CHECKSUM in pb
+                assert PB.NINE_VARINT in pb
                 assert pb[PB.OFTEN_EMPTY]  # Not empty in this case.
                 assert pb[PB.SOMETIMES_9] == 9
 
-        print("Read %i packets, %i w/o storm data" % (i, no_storm_packet))
+        print(
+            "Read %i packets, %i w/o storm data, %i with pb key 9"
+            % (i, no_storm_packet, packet_with_nine)
+        )
